52bb6a5cbc4e2f8595c6836a140235c28bebc84e
dover20 <doverlin17@gmail.com>
2020-03-13 15:10:49 -0400

    new features

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //NASA InSight API gets temperature, wind, and pressure data from the InSight lander.
 //jshint -W104
+//jshint -W119
 
 function nasaAPI() {
     fetch('https://api.nasa.gov/insight_weather/?api_key=PbSTQLOONNboqZtLh7aOkvfuXp5QMIokby4xl5Pj&feedtype=json&ver=1.0')
@@ -31,10 +32,9 @@ function nasaAPI() {
             let options = document.querySelector("#selections");
     
             let selectionInput = options.value;
-            
-            //put data into variables
             let selectedDay = selectionInput;
-            let lastDate = data[selectedDay].Last_UTC;
+
+            //put data into variables
             let averageTemp = data[selectedDay].AT.av;
             let minimumTemp = data[selectedDay].AT.mn;
             let maximumTemp = data[selectedDay].AT.mx;
@@ -42,15 +42,17 @@ function nasaAPI() {
             let averageWindSpeed = data[selectedDay].HWS.av;
             let minimumWindSpeed = data[selectedDay].HWS.mn;
             let maximumWindSpeed = data[selectedDay].HWS.mx;
-            let windSamples = data[selectedDay].HWS.ct;
             let avgWindDirection = data[selectedDay].WD.most_common.compass_point;
-        
-            //convert UTC time to date object
-            let currentDateObject = new Date(lastDate);
-            let year = currentDateObject.getUTCFullYear();
-            let month = currentDateObject.toLocaleString('default', { month: 'long' });
-            let day = currentDateObject.getUTCDate();
-            let fullDate = month + ", " + day + ", " + year;
+            let windSamples = data[selectedDay].HWS.ct;
+
+            //Check for next update
+            let nextDay = data.validity_checks.sols_checked[7];
+            let validHours = data.validity_checks[nextDay].AT.sol_hours_with_data.length;
+            let hoursNeeded = 18;
+            let howManyHoursLeft = hoursNeeded - validHours;
+            if( validHours < 18 ) {
+                console.log(howManyHoursLeft + " hours until next weather report.");
+            }
         
             //make first letter of season capitalized
             let currentSeason = data[selectedDay].Season;
@@ -58,20 +60,21 @@ function nasaAPI() {
             let restOfChars = currentSeason.slice(1,30);
             let season = firstChar.toUpperCase() + restOfChars.toLowerCase();
             
-            //print variables to HTML
-        
-            //document.querySelector('.dateInfo').innerHTML = fullDate;
+            //print data to HTML
             document.querySelector('.solInfo').innerHTML = selectedDay + " sols";
             document.querySelector('.maxTempInfo').innerHTML = CelciusToFahren(maximumTemp).toFixed(2) + "F";
             document.querySelector('.minTempInfo').innerHTML = CelciusToFahren(minimumTemp).toFixed(2) + "F";
             document.querySelector('.tempInfo').innerHTML = CelciusToFahren(averageTemp).toFixed(2) + "F";
-            document.querySelector('.seasonInfo').innerHTML = season;
             document.querySelector('.sampleInfo').innerHTML = dailySamples.toLocaleString();
-            document.querySelector('.minimumWindSpeed').innerHTML = minimumWindSpeed + " MPH";
+            document.querySelector('.minimumWindSpeed').innerHTML = minimumWindSpeed.toFixed(2) + " MPH";
             document.querySelector('.maximumWindSpeed').innerHTML = maximumWindSpeed.toFixed(2) + " MPH";
-            document.querySelector('.avgWindSpeed').innerHTML = averageWindSpeed + " MPH";
+            document.querySelector('.avgWindSpeed').innerHTML = averageWindSpeed.toFixed(2) + " MPH";
+            document.querySelector('.avgWindDirection').innerHTML = avgWindDirection;
             document.querySelector('.windSamplesInfo').innerHTML = windSamples.toLocaleString();
-            document.querySelector('.avgWindDirection').innerHTML = avgWindDirection; 
+            document.querySelector('.seasonInfo').innerHTML = season;
+
+            document.querySelector('#nextUpdate').innerHTML = "T-minus " + 
+            `<span class="bigText">${howManyHoursLeft}</span>` + " hours until next weather report.";
     });
 }
 
