66f0bb331b9524a458e60ae19610708dc9a7ba2b
dover20 <doverlin17@gmail.com>
2020-03-13 01:18:41 -0400

    added date selection, improved styling, etc

--- index.js
+++ index.js
@@ -1,52 +1,83 @@
 //NASA InSight API gets temperature, wind, and pressure data from the InSight lander.
 //jshint -W104
-fetch('https://api.nasa.gov/insight_weather/?api_key=PbSTQLOONNboqZtLh7aOkvfuXp5QMIokby4xl5Pj&feedtype=json&ver=1.0')
-.then( function(response) {
-    return response.json();
-})
-.then(function(data) {
-    let latestDay = data.sol_keys[6];
-    let averageTemp = data[latestDay].AT.av;
-    let minimumTemp = data[latestDay].AT.mn;
-    let maximumTemp = data[latestDay].AT.mx;
-    let dailySamples = data[latestDay].AT.ct;
-    let lastDate = data[latestDay].Last_UTC;
 
-    let currentSeason = data[latestDay].Season;
-    let firstChar = currentSeason.slice(0,1);
-    let restOfChars = currentSeason.slice(1,30);
-    let season = firstChar.toUpperCase() + restOfChars.toLowerCase();
+function nasaAPI() {
+    fetch('https://api.nasa.gov/insight_weather/?api_key=PbSTQLOONNboqZtLh7aOkvfuXp5QMIokby4xl5Pj&feedtype=json&ver=1.0')
+    .then( function(response) {
+        return response.json();
+    })
+    .then(function(data) {
     
+        let selectElement = document.querySelector("#selections");
+    
+        for (let i = 6; i > -1; i--) {
+            let lastSevenSols = data.sol_keys[i];
+            let all7Dates = data[lastSevenSols].Last_UTC;
+    
+            //convert UTC time to date object
+            let currentDateObject = new Date(all7Dates);
+            let year = currentDateObject.getUTCFullYear();
+            let month = currentDateObject.toLocaleString('default', { month: 'long' });
+            let day = currentDateObject.getUTCDate();
+            let allDates = month + ", " + day + ", " + year;
+    
+            let checkForOptions = document.getElementsByTagName("option").length;
 
-    //Dates and stuff
-    let currentDateObject = new Date(lastDate);
-    let year = currentDateObject.getUTCFullYear();
-    let month = currentDateObject.getUTCMonth() + 1;
-    let day = currentDateObject.getUTCDate();
-    let fullDate = month + "-" + day + "-" + year;
+            if ( checkForOptions <= 6) {
+                selectElement.add( new Option( allDates, data.sol_keys[i]) );
+            }
+        }
+            //check what option is selected
+            let options = document.querySelector("#selections");
+    
+            let selectionInput = options.value;
+            
+            //put data into variables
+            let selectedDay = selectionInput;
+            let lastDate = data[selectedDay].Last_UTC;
+            let averageTemp = data[selectedDay].AT.av;
+            let minimumTemp = data[selectedDay].AT.mn;
+            let maximumTemp = data[selectedDay].AT.mx;
+            let dailySamples = data[selectedDay].AT.ct;
+            let averageWindSpeed = data[selectedDay].HWS.av;
+            let minimumWindSpeed = data[selectedDay].HWS.mn;
+            let maximumWindSpeed = data[selectedDay].HWS.mx;
+            let windSamples = data[selectedDay].HWS.ct;
+            let avgWindDirection = data[selectedDay].WD.most_common.compass_point;
+        
+            //convert UTC time to date object
+            let currentDateObject = new Date(lastDate);
+            let year = currentDateObject.getUTCFullYear();
+            let month = currentDateObject.toLocaleString('default', { month: 'long' });
+            let day = currentDateObject.getUTCDate();
+            let fullDate = month + ", " + day + ", " + year;
+        
+            //make first letter of season capitalized
+            let currentSeason = data[selectedDay].Season;
+            let firstChar = currentSeason.slice(0,1);
+            let restOfChars = currentSeason.slice(1,30);
+            let season = firstChar.toUpperCase() + restOfChars.toLowerCase();
+            
+            //print variables to HTML
+        
+            //document.querySelector('.dateInfo').innerHTML = fullDate;
+            document.querySelector('.solInfo').innerHTML = selectedDay + " sols";
+            document.querySelector('.maxTempInfo').innerHTML = CelciusToFahren(maximumTemp).toFixed(2) + "F";
+            document.querySelector('.minTempInfo').innerHTML = CelciusToFahren(minimumTemp).toFixed(2) + "F";
+            document.querySelector('.tempInfo').innerHTML = CelciusToFahren(averageTemp).toFixed(2) + "F";
+            document.querySelector('.seasonInfo').innerHTML = season;
+            document.querySelector('.sampleInfo').innerHTML = dailySamples.toLocaleString();
+            document.querySelector('.minimumWindSpeed').innerHTML = minimumWindSpeed + " MPH";
+            document.querySelector('.maximumWindSpeed').innerHTML = maximumWindSpeed.toFixed(2) + " MPH";
+            document.querySelector('.avgWindSpeed').innerHTML = averageWindSpeed + " MPH";
+            document.querySelector('.windSamplesInfo').innerHTML = windSamples.toLocaleString();
+            document.querySelector('.avgWindDirection').innerHTML = avgWindDirection; 
+    });
+}
 
-    document.querySelector('.dateInfo').innerHTML = fullDate;
-    document.querySelector('.maxTempInfo').innerHTML = CelciusToFahren(maximumTemp).toFixed(2) + "F";
-    document.querySelector('.minTempInfo').innerHTML = CelciusToFahren(minimumTemp).toFixed(2) + "F";
-    document.querySelector('.tempInfo').innerHTML = CelciusToFahren(averageTemp).toFixed(2) + "F";
-    document.querySelector('.seasonInfo').innerHTML = season;
-    document.querySelector('.sampleInfo').innerHTML = dailySamples.toLocaleString();
-});
+nasaAPI();
 
 //Converts input to celcius
 function CelciusToFahren(temp) {
     return 1.8 * temp + 32;
 }
-
-
-/* Console logs
-
-console.log("Current date: " + currentDateObject);
-    console.log("Maximum temp: " + CelciusToFahren(maximumTemp).toFixed(2) + "F" );
-    console.log("Minimum temp: " + CelciusToFahren(minimumTemp).toFixed(2) + "F" );
-    console.log("Average temp: " + CelciusToFahren(averageTemp).toFixed(2) + "F");
-    console.log("The current season in Elysium Planitia is " + currentSeason);
-    console.log("Number of samples collected on this day " + dailySamples);
-    console.log(fullDate);
-
-    */
